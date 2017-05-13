d54a2d3d85f05336808f69bf66fc7ce6e50a17ad
digitalerwandel <julius@digitalerwandel.de>
2017-05-13 13:31:54 +0200

    update script

--- ml-workshop-script.R
+++ ml-workshop-script.R
@@ -16,63 +16,18 @@ setwd("/Users/juliustroeger/Projekte/")
 # Zweitstimmen der Bundestagswahl 2013 und soziodemographische Daten laden
 d <- read.csv('machine-learning/ml-workshop-daten.csv') 
 
+# Parteistimmen faktorisieren: Hat z.B. die AfD die Fünf-Prozent-Hürde geschafft: Dummyvariable ja = 1, nein = 0)
+d$factor_party <- d$AfD
+d$factor_party <- factor(with(d,ifelse((factor_party <= 5),0,1)))
 
-# Spalte mit Parteien einfügen, die Landkreise gewonnen haben
-which(colnames(d)=="factor_CDU")
-z <- apply(d[5:34],1,which.max) 
-d$Gewinner <- names(d[5:34])[z]
-rm(z)
-
-# Parteistimmen faktorisieren: Weniger oder mehr als x Prozent
-factor_party <- d[5:34] 
-colnames(factor_party) <- paste("factor", colnames(factor_party), sep = "_")
-d <- cbind(d, factor_party)
-rm(factor_party)
-
-# Parteistimmen faktorisieren (Ist Partei X stärkste Kraft im Briefwahlbezirk? Dummyvariable ja = 1, nein = 0)
-# Codierung ja = 1 nein = 0, wenn: Fünf-Prozent-Hürde für FDP und NPD, Zweistellig für AfD, d$Gewinner = SPD, CDU, Grüne, Linke
-for(i in 68:ncol(d)){
-  for(k in 1:nrow(d)){
-    if(colnames(d[i]) == "factor_AfD"){
-      if(d[k,i] >= 5){
-        d[k,i] <- 1
-      } else{d[k,i] <- 0}
-    } else{
-      if(colnames(d[i]) == "factor_SPD" | colnames(d[i]) == "factor_CDU" | colnames(d[i]) == "factor_CSU"){
-        x <- unlist(strsplit(colnames(d[i]), "_"))[2]
-        if(d$Gewinner[k] == x){
-          d[k,i] <- 1
-        } else{d[k,i] <- 0} 
-      } else{
-        if(d[k,i] >= 5){
-          d[k,i] <- 1
-        } else{d[k,i] <- 0}  
-      }
-    }
-  }
-}
-
-d$factor_AfD <- as.factor(d$factor_AfD)
-
-table(d$factor_AfD) # Check: In so vielen Briefwahlbezirken konnten Parteien gewinnen / Über die 5-Prozent-Hürde springen
-prop.table(table(d$factor_AfD))*100 # Ausgabe in Prozent
+table(d$factor_party) # Check: In so vielen Briefwahlbezirken konnten Parteien gewinnen / Über die 5-Prozent-Hürde springen
+prop.table(table(d$factor_party))*100 # Ausgabe in Prozent
 
 #######################################################################################
 # MODEL BUILDING: DECISION TREE MIT CARET/RPART UND 10-FOLD-CROSSVALIDATION ERSTELLEN #
 #######################################################################################
 
-# # Lineare Regression
-# plot(d$GRÜNE ~ d$Art)
-# fit_lin <- lm(d$GRÜNE ~ d$Medianeinkommen)
-# abline(fit_lin)
-# summary(fit_lin)
-# 
-# # Multiple Regression
-# fit_lin_multi <- lm(d$GRÜNE_rel ~ d$Mietpreis + d$Medianeinkommen + d$Haus..und.Sperrmüll)
-# summary(fit_lin_multi)
-# # TO-DO: Kollinearität checken / wie damit umgehen
-
-variables <- factor_AfD ~ Wahlbeteiligung + ost_west + Medianeinkommen + Arbeitslosenquote + Ausländeranteil + Gesamtwanderungssaldo + Geborene + Gestorbene + Schulabgänger.mit.Hauptschulabschluss + Schulabgänger.mit.Hochschulreife + Schulabgänger.ohne.Abschluss + Alle_Anzeigen + Anzeigen_Ausländerquote + Ladendiebstahl + Wohnungseinbruch + Autoklau + Fahrradklau + Betrug + Beförderungserschleichung + Widerstand_Polizei + Brandstiftung + Rauschgift + Mord_Totschlag + Mietpreis + Mietsteigerung + Kaufpreis + PKW + Krafträder + Cabrios + Elektroautos + Sportwagen + Kirchenmitglieder
+variables <- factor_party ~ Wahlbeteiligung + ost_west + Medianeinkommen + Arbeitslosenquote + Ausländeranteil + Mietpreis
 
 d_clean <- na.omit(d) # NA-Werte löschen (führt zu kleinerem Datensatz. Besser Imputation?)
 
@@ -80,7 +35,7 @@ tc <- trainControl(method = "repeatedcv", number = 10, repeats = 10) # Daten zuf
 train.rpart <- train(variables, data=d_clean, method="rpart", tuneLength=10, metric="Accuracy", trControl=tc) # Metric "Accuracy" für Classification, "Rsquared" für Regression
 
 plot(train.rpart$finalModel, uniform=TRUE) # Baum plotten
-text(train.rpart$finalModel, all=TRUE) # Text plotten
+text(train.rpart$finalModel, use.n=TRUE, all=TRUE, cex=.7) # Text plotten
 
 ###############
 # EVALUIERUNG #
@@ -88,5 +43,6 @@ text(train.rpart$finalModel, all=TRUE) # Text plotten
 
 train.rpart # Accuracy der Crossvalidation überprüfen (Hier nur 76 Prozent)
 
-# TO-DO: Diesen Punkt detaillierter. Wie Modell tunen? Andere Algorithmen testen und vergleichen? RSME nötig?
+# TO-DO: Diesen Punkt detaillierter. Wie Modell tunen? Andere Algorithmen testen und vergleichen? RSME nötig? Was sind die wichtigsten Einflussfaktoren? Was sagt uns Decision Tree jetzt genau? Doch lieber Gemeinden?
+# Naive Bayes oder Neuronales Netz mit Tensorflow spannender?
 
