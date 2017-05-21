8d661e0986180ba4026f110ea08133276618207e
digitalerwandel <julius@digitalerwandel.de>
2017-05-21 11:45:44 +0200

    update skript

--- experimente/mds.R
+++ experimente/mds.R
@@ -2,9 +2,9 @@
 
 d <- read.csv('machine-learning/ml-workshop-daten.csv') 
 #d <- d[, !(colnames(d) %in% c("Name", "Landbevölkerung", "Landbevölkerung.1", "Ältere.Landbevölkerung", "Jüngere.Dorfbewohner" ))]
-d <- d[, !(colnames(d) %in% c("KRS","Art","ost_west"))]
+d$Union <- d$CDU + d$CSU
+d <- d[, !(colnames(d) %in% c("Name","KRS","Art","ost_west","CDU","CSU"))]
 d <- na.omit(d)
-
 d_dist <- dist(d) # euclidean distances between the rows
 fit <- cmdscale(d_dist,eig=TRUE, k=2) # k is the number of dim
 fit # view results
@@ -16,11 +16,29 @@ plot(x, y, xlab="Coordinate 1", ylab="Coordinate 2",
      main="Metric	MDS")
 text(x, y, labels = d$Name, cex=.7)
 
+## PCA
+d <- read.csv('machine-learning/ml-workshop-daten.csv') 
+d <- subset(d, select = -c(Art,Name,ost_west)) 
+d <- na.omit(d)
+
+fit_pca <- prcomp(d, retx=TRUE, center=TRUE, scale=TRUE)
+expl.var <- round(fit_pca$sdev^2/sum(fit_pca$sdev^2)*100) # percent explained variance
+expl.var[1]
+
+summary(fit_pca)
+fit_pca$loadings
+fit_pca$scores
+
+plot(fit_pca, type = "l")
+biplot(fit_pca)
+plot(prcomp(scale(fit_pca)))
+
 ## KMEANS CLUSTER
 
 d <- read.csv('machine-learning/ml-workshop-daten.csv')
 #d <- d[, !(colnames(d) %in% c("Name", "Landbevölkerung", "Landbevölkerung.1", "Ältere.Landbevölkerung", "Jüngere.Dorfbewohner" ))]
-d <- d[, !(colnames(d) %in% c("KRS","Art","ost_west"))]
+d$Union <- d$CDU + d$CSU
+d <- d[, !(colnames(d) %in% c("KRS","Art","ost_west","CDU","CSU"))]
 d <- na.omit(d)
 d_scale <- scale(d[3:32]) # standardize variables
 is.na(d_scale)
--- ml-workshop-script.R
+++ ml-workshop-script.R
@@ -1,50 +1,47 @@
-#######################
-# PAKETE INSTALLIEREN #
-#######################
-
+# Pakete installieren
 #install.packages('needs')
 library(needs)
-needs(caret)
-
-################################################################################
-# FEATURE ENGINEERING: DATEN EINLESEN, MERGEN, NORMALISIEREN UND FAKTORISIEREN #
-################################################################################
+needs(caret,car)
 
 # Arbeitsverzeichnis einrichten
 setwd("/Users/juliustroeger/Projekte/")
 
-# Zweitstimmen der Bundestagswahl 2013 und soziodemographische Daten laden
+#######################
+# FEATURE ENGINEERING #
+#######################
+
+# Zweitstimmen der Bundestagswahl 2013 mit soziodemographische Daten laden
 d <- read.csv('machine-learning/ml-workshop-daten.csv') 
 
 # Parteistimmen faktorisieren: Hat z.B. die AfD die Fünf-Prozent-Hürde geschafft: Dummyvariable ja = 1, nein = 0)
 d$factor_party <- d$AfD
 d$factor_party <- factor(with(d,ifelse((factor_party <= 5),0,1)))
 
-table(d$factor_party) # Check: In so vielen Briefwahlbezirken konnten Parteien gewinnen / Über die 5-Prozent-Hürde springen
-prop.table(table(d$factor_party))*100 # Ausgabe in Prozent
-
-#######################################################################################
-# MODEL BUILDING: DECISION TREE MIT CARET/RPART UND 10-FOLD-CROSSVALIDATION ERSTELLEN #
-#######################################################################################
-
-# Erklärung: Interpretierbar vs. Genauigkeit je nach Anwendungsfall
+##################
+# MODEL BUILDING #
+##################
 
 variables <- factor_party ~ Wahlbeteiligung + ost_west + Medianeinkommen + Arbeitslosenquote + Ausländeranteil + Mietpreis
 
-d_clean <- na.omit(d) # NA-Werte löschen (führt zu kleinerem Datensatz. Besser Imputation?)
+# fit <- lm(AfD ~ Wahlbeteiligung + ost_west + Medianeinkommen + Arbeitslosenquote + Ausländeranteil + Mietpreis, data=d)
+# summary(fit)
+# vif(fit) # TO-DO: Wie kann man das interpretieren?
+
+d_clean <- na.omit(d) # NA-Werte löschen (TO-DO: führt zu kleinerem Datensatz. Besser Imputation?)
 
 tc <- trainControl(method = "repeatedcv", number = 10, repeats = 10) # Daten zufällig in zehn ähnlich große Blöcke aufteilen
 train.rpart <- train(variables, data=d_clean, method="rpart", tuneLength=10, metric="Accuracy", trControl=tc) # Metric "Accuracy" für Classification, "Rsquared" für Regression
 
-# OHNE CROSSVALIDATION TESTEN
-
-plot(train.rpart$finalModel, uniform=TRUE) # Baum plotten
+plot(train.rpart$finalModel, uniform=TRUE, margin=0.2) # Baum plotten
 text(train.rpart$finalModel, use.n=TRUE, all=TRUE, cex=.7) # Text plotten
+
+varImp(train.rpart) # TO-DO: Warum in umgekehrter Reihenfolge?
+#plot(varImp(train.rpart))
 
 ###############
 # EVALUIERUNG #
 ###############
 
-train.rpart # Accuracy der Crossvalidation überprüfen (Hier nur 76 Prozent)
-# Fällt prinzipiell weg hier
+train.rpart # Accuracy des Modells überprüfen
+# TO-DO: Interpretierbar vs. Genauigkeit je nach Anwendungsfall / Ohne Crossvalidation gegenchecken?
 
